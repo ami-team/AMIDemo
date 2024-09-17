3b50ecfde6373af5eebe63bd68ccc1d13eaa6849
flambert <fabian.lambert@lpsc.in2p3.fr>
2024-09-17 16:21:38 +0200

    post install

---
+++ postInstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo cp ./home.html ./data-web-framework/docs/home.html
+sudo chmod 666 ./data-web-framework/docs/home.html
+
+sudo cp ./mql_constraint.html ./data-web-framework/docs/mql_constraint.html
+sudo chmod 666 ./data-web-framework/docs/mql_constraint.html
+
+sudo cp ./mql_isolation.html ./data-web-framework/docs/mql_isolation.html
+sudo chmod 666 ./data-web-framework/docs/mql_isolation.html
+
+sudo cp ./schema.png ./data-web-framework/docs/schema.png
+sudo chmod 666 ./data-web-framework/docs/schema.png
